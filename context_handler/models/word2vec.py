008e4836c4d4b8ddd849ad2f3abb78997ed493d6
Pedro Cleto <cl3t0.sh@gmail.com>
2023-05-28 01:24:18 -0300

    feat: using classes to create the models

--- context_handler/models/word2vec.py
+++ context_handler/models/word2vec.py
@@ -1,32 +1,45 @@
 from gensim.models.keyedvectors import KeyedVectors
 import json
-import os
 import typing as t
 import numpy as np
-
-USE_MOCKED_MODEL = os.environ.get("USE_MOCKED_MODEL", "FALSE") == "TRUE"
-print(f"Using mocked model: {USE_MOCKED_MODEL}")
-
-
-def load_model() -> t.Dict[str, np.ndarray]:
-    return KeyedVectors.load("word2vec-google-news-300.model")
+from google.cloud import storage
+import os
 
 
-def load_mocked_model() -> t.Dict[str, np.ndarray]:
-    mocked_model = json.load(open("mocked_word2vec.json", "r"))
+class Word2Vec:
+    def __init__(self, use_mocked_model: bool) -> None:
+        print(f"Using mocked model: {use_mocked_model}")
+        self.model = (
+            Word2Vec.load_model()
+            if not use_mocked_model
+            else Word2Vec.load_mocked_model()
+        )
 
-    class MockedModel(dict):
-        def __getitem__(self, item) -> np.ndarray:
-            return np.array(mocked_model[item])
+    @staticmethod
+    def load_model() -> t.Dict[str, np.ndarray]:
+        filename = "word2vec-google-news-300.model"
+        if filename in os.listdir():
+            return KeyedVectors.load(filename)
+        path = f"/tmp/{filename}"
+        bucket = storage.Client().get_bucket("grido-ml-models")
+        blob_model = bucket.blob(f"{filename}")
+        blob_vectors = bucket.blob(f"{filename}.vectors.npy")
+        blob_model.download_to_filename(path)
+        blob_vectors.download_to_filename(f"{path}.vectors.npy")
+        return KeyedVectors.load(path)
 
-    return MockedModel()
+    @staticmethod
+    def load_mocked_model() -> t.Dict[str, np.ndarray]:
+        mocked_model = json.load(open("mocked_word2vec.json", "r"))
 
+        class MockedModel(dict):
+            def __getitem__(self, item) -> np.ndarray:
+                return np.array(mocked_model[item])
 
-MODEL = load_model() if not USE_MOCKED_MODEL else load_mocked_model()
+        return MockedModel()
 
-
-def calc(word: str) -> t.Optional[np.ndarray]:
-    try:
-        return MODEL[word]
-    except:
-        return None
+    def calc(self, word: str) -> t.Optional[np.ndarray]:
+        try:
+            return self.model[word]
+        except:
+            return None
