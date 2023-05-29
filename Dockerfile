29932a0838211859aa542211f41c5ad95b0ad31b
Pedro Cleto <cl3t0.sh@gmail.com>
2023-05-28 21:27:19 -0300

    feat: add cloud run deploy

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.10-slim
+
+ENV PYTHONUNBUFFERED True
+
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+COPY . ./
+
+ENV CODE_ENV DEV
+ENV PORT 8080
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+# As an example here we're running the web service with one worker on uvicorn.
+CMD exec uvicorn context_handler.app:app --host 0.0.0.0 --port ${PORT} --workers 1
--- context_handler/setup.py
+++ context_handler/setup.py
@@ -3,21 +3,34 @@ from context_handler.entities import DatabaseCredentials, SetupData
 from dotenv import load_dotenv
 import os
 
+CODE_ENV = os.environ.get("CODE_ENV") or "LOCAL"
 load_dotenv()
 
-USE_MOCKED_MODEL = os.environ.get("USE_MOCKED_MODEL", "FALSE") == "TRUE"
-DATABASE_TYPE = os.environ.get("DATABASE_TYPE") or _raise(
-    "DATABASE_TYPE env var should be set"
-)
-DB_HOST = os.environ.get("DB_HOST") or _raise("DB_HOST env var should be set")
-DB_PORT = os.environ.get("DB_PORT") or _raise("DB_PORT env var should be set")
-DB_USER = os.environ.get("DB_USER") or _raise("DB_USER env var should be set")
-DB_PASS = os.environ.get("DB_PASS") or _raise("DB_PASS env var should be set")
+
+def get_env(var: str) -> str:
+    if CODE_ENV == "LOCAL":
+        return os.environ.get(var) or _raise(f"{var} env var should be set")
+    elif CODE_ENV == "DEV":
+        return os.environ.get(f"DEV_{var}") or _raise(f"{var} env var should be set")
+    else:
+        return os.environ.get(f"PRD_{var}") or _raise(f"{var} env var should be set")
+
+
+USE_MOCKED_MODEL = get_env("USE_MOCKED_MODEL")
+DATABASE_TYPE = get_env("DATABASE_TYPE")
+DB_HOST = get_env("DB_HOST")
+DB_PORT = get_env("DB_PORT")
+DB_USER = get_env("DB_USER")
+DB_PASS = get_env("DB_PASS")
 
 setup_data = SetupData(
     database_type=DATABASE_TYPE,
-    use_mocked_model=USE_MOCKED_MODEL,
+    use_mocked_model=USE_MOCKED_MODEL == "TRUE",
     database_credentials=DatabaseCredentials(
         host=DB_HOST, port=int(DB_PORT), username=DB_USER, password=DB_PASS
     ),
 )
+
+print(f"code_env: {CODE_ENV}")
+print(f"database_type: {setup_data.database_type}")
+print(f"use_mocked_model: {setup_data.use_mocked_model}")
