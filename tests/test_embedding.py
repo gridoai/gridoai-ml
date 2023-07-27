8c0f749bbc86cf3ed9c4a4df643182c428ba09d1
Pedro Cleto <cl3t0.sh@gmail.com>
2023-07-27 12:23:52 -0300

    fix: testing with post instead of get

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -5,5 +5,5 @@ HOST = "http://127.0.0.1:8000"
 
 
 def test_embeddding() -> None:
-    r = requests.get(f"{HOST}/embed", json={"text": "cats are better than people"})
+    r = requests.post(f"{HOST}/embed", json={"texts": ["cats are better than people"]})
     assert r.status_code == 200
