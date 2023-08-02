181e2e29bf112e47f58c2e1e1f2ba41229a81de1
Pedro Cleto <cl3t0.sh@gmail.com>
2023-08-02 12:38:01 -0300

    fix: tests

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -5,5 +5,11 @@ HOST = "http://127.0.0.1:8000"
 
 
 def test_embeddding() -> None:
-    r = requests.post(f"{HOST}/embed", json={"texts": ["cats are better than people"]})
+    r = requests.post(
+        f"{HOST}/embed",
+        json={
+            "texts": ["cats are better than people"],
+            "model": "multilingual-e5-base",
+        },
+    )
     assert r.status_code == 200
