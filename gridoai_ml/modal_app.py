b0dd7dcccaa58963cef91254cf823ba412d88a68
davisuga <daviciencia1@gmail.com>
2023-09-05 19:23:32 -0300

    perf optimizations + modal deploy

--- gridoai_ml/modal_app.py
+++ gridoai_ml/modal_app.py
@@ -1,17 +1,35 @@
-from modal import Stub, web_endpoint
+from modal import Stub, web_endpoint, Secret, Image
+from modal.gpu import T4
 from gridoai_ml.entities import Document, EmbeddingPayload
 
 
-stub = Stub("example-get-started")
+def download_model():
+    from sentence_transformers import SentenceTransformer
+    get_model(setup_data)
 
+image = Image.debian_slim().pip_install(
+    # scraping pkgs
+    "python-dotenv~=1.0.0",
+    "requests~=2.31.0",
+    "numpy",
+    "sentence_transformers"
+).env(
+    {"SENTENCE_TRANSFORMERS_HOME":"./.cache/",
+    "EMBEDDING_MODEL":"multilingual-e5-base"},
+).run_function(download_model)
+stub = Stub(
+    name="example-get-started",
+    image=image,
+)
 
-@stub.function()
+from gridoai_ml.setup import setup_data
+from gridoai_ml.text_embedding_models import get_model
+model = get_model(setup_data)
+
+@stub.function(secret=Secret.from_name("my-custom-secret"), gpu='t4', allow_concurrent_inputs=3, memory=1024)
 @web_endpoint("POST")
 async def embed(payload: EmbeddingPayload):
-    from gridoai_ml.setup import setup_data
-    from gridoai_ml.text_embedding_models import get_model
 
-    model = get_model(setup_data)
     if payload.model != setup_data.embedding_model:
         return {"message": "model not available"}
     try:
