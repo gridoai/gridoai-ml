2423f754fded51513e63911ce000e200bc0798d6
davisuga <daviciencia1@gmail.com>
2023-09-07 15:48:15 -0300

    import fix

--- gridoai_ml/text_embedding_models/__init__.py
+++ gridoai_ml/text_embedding_models/__init__.py
@@ -1,20 +1,18 @@
 from gridoai_ml.text_embedding_models.abs_model import AbsTextEmbeddingModel
-# from gridoai_ml.text_embedding_models.doc2vec import Doc2Vec
-from gridoai_ml.text_embedding_models.mocked import MockedModel
-# from gridoai_ml.text_embedding_models.instructor import InstructorModel
-from gridoai_ml.text_embedding_models.multilingual_e5_base import (
-    MultilingualE5BaseModel,
-)
 from gridoai_ml.entities import SetupData
-import typing as t
-
-MODELS: t.Dict[str, t.Type[AbsTextEmbeddingModel]] = {
-    "mocked": MockedModel,
-    # "doc2vec": Doc2Vec,
-    # "instructor": InstructorModel,
-    "multilingual-e5-base": MultilingualE5BaseModel,
-}
-
 
 def get_model(setup_data: SetupData) -> AbsTextEmbeddingModel:
-    return MODELS[setup_data.embedding_model]()
+    match setup_data.embedding_model:
+        case "mocked":
+            from gridoai_ml.text_embedding_models.mocked import MockedModel
+            return MockedModel()
+        case "multilingual-e5-base":
+            from gridoai_ml.text_embedding_models.multilingual_e5_base import MultilingualE5BaseModel
+            return MultilingualE5BaseModel()
+        case "doc2vec":
+            from gridoai_ml.text_embedding_models.doc2vec import Doc2Vec
+            return Doc2Vec()
+        case "instructor":
+            from gridoai_ml.text_embedding_models.instructor import InstructorModel
+            return InstructorModel()
+
